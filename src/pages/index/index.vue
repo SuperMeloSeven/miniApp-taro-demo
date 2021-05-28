6ae12de3e01268e491ad95a23e00f4efe392c7af
huzhilei <924524413@qq.com>
2021-05-28 19:51:08 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <text>第一次使用taro</text>
+  </view>
+</template>
+
+<script>
+import './index.less'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
