15e1bdb9ff7236875246ba67e1ead41934101e76
huzhilei <924524413@qq.com>
2022-06-01 21:30:16 +0800

    init

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,6 +2,12 @@
   <view class="index">
     <text>{{ msg }}</text>
     <text>第一次使用taro</text>
+    <text class="font-style">
+      第二次使用使用taro
+    </text>
+    <view>
+      多端同步调试
+    </view>
   </view>
 </template>
 
